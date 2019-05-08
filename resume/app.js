619fc492f7ca1cb9b2b4e2cbf7358c69ee7b3cf9
Kevin Song <aero.no1.ks@gatech.edu>
2019-05-08 17:02:38 -0400

    resume download

---
+++ resume/app.js
@@ -0,0 +1,5 @@
+$(() => {
+  $('#pdf').click(() => {
+    window.open('https://v2018.api2pdf.com/chrome/url?url=https://aero-no1-ks.github.io/resume&apikey=402bfc4f-93da-4551-b829-f836a4108361')
+  })
+})
